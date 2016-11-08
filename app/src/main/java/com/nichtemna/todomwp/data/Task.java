6d37ff6a5e1d0bb11d3de51d0175b9a4447398ab
Lina.Shyshova <lina.shyshova@echostar.com>
2016-11-08 17:57:28 +0200

    Finished UI

--- app/src/main/java/com/nichtemna/todomwp/data/Task.java
+++ app/src/main/java/com/nichtemna/todomwp/data/Task.java
@@ -4,6 +4,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
+import com.google.common.base.Strings;
+
 import java.util.Objects;
 import java.util.UUID;
 
@@ -89,6 +91,11 @@ public class Task {
         } else {
             return mDescription;
         }
+    }
+
+    public boolean isEmpty() {
+        return Strings.isNullOrEmpty(mTitle) &&
+                Strings.isNullOrEmpty(mDescription);
     }
 
     @Nullable
